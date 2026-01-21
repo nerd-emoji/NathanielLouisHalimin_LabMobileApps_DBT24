8531b7841c6360d8d869641bd9064787140a44b9
nerd-emoji <nathaniellouish@gmail.com>
2026-01-21 14:03:48 +0700

    q4

---
+++ Quiz1_Yang_Seru_sekali/no4.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+import 'dart:math';
+
+void main () {
+  bool prime = true;
+  stdout.write('Masukan angka: ');
+  String? input = stdin.readLineSync();
+  int? bilangan = int.tryParse(input!);
+  for (var i = 2; i <bilangan!; i++){
+    if (bilangan % i == 0){
+      prime = false;
+      break;
+    }
+  }
+  if (prime == true){
+    print('$bilangan adalah bilangan prima');
+  } else {
+    print('$bilangan bukan bilangan prima');
+  }
+}
